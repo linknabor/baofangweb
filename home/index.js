a7c0bb19efe484cee1410fdab44857d4636d2996
tw <ttw789@gmail.com>
2016-11-25 18:28:27 +0800

    add page 14

--- home/index.js
+++ home/index.js
@@ -6,13 +6,22 @@
 	function query(){
 		common.invokeApi("GET","pageconfig2/13",null,null,function(n){
 			alert(n);
-			//o.banners = n.banners;
+			o.banners = n.result;
 			//o.jingxuans=n.jingxuans;
 			initSwiper();
 		},function(){
 			alert("页面获取信息错误，请稍后重试！");
 		})
-
+	}
+	function query14(){
+		common.invokeApi("GET","pageconfig2/14",null,null,function(n){
+			alert(n);
+			o.pics14= n.result;
+			//o.jingxuans=n.jingxuans;
+			initSwiper();
+		},function(){
+			alert("页面获取信息错误，请稍后重试！");
+		})
 	}
     var o = avalon.define({
         $id: "root",
@@ -24,6 +33,7 @@
         }
     });
 	query();
+	query14();
     initWechat(['onMenuShareTimeline','onMenuShareAppMessage']);
     initShareConfig("鲜花、汽车、健康、维修、洗衣、家政...点亮生活，尽在我家大楼管家服务！",MasterConfig.C("basePageUrl")+"home/index.html?v=20160229",MasterConfig.C("basePageUrl")+"/static/images/share_logo1.png","足不出户即享简单便捷的居家生活");
     avalon.scan(document.body),

722f97d1b7c764e727125cc4f0c02e0d66427c02
zhaoshiwei <861812342.@qq.com>
2019-04-23 10:58:27 +0800

    贵州保洁测试2

--- static/config.js
+++ static/config.js
@@ -2,7 +2,7 @@ var url=/127|test/.test(location.origin)?'https://test.e-shequ.com':
     /uat/.test(location.origin)?'https://uat.e-shequ.com':
     'https://www.e-shequ.com';
 var name='/baofang';
-var namepay='/pay';
+var namepay='/baofang/weixin';
 window.config={
      
     //公共的请求地址
